1970ea6d638b7a019d0ce027ee405cafb7876817
hanhEvizi <hanh.ngo@evizi.com>
2023-05-08 17:44:40 +0700

    fixing double quote

--- helper/constants.ts
+++ helper/constants.ts
@@ -3,4 +3,4 @@ require("dotenv").config({ path: ".env.qa" });
 export const BASE_URL = "https://api.thedogapi.com";
 export const X_API_KEY = process.env.X_API_KEY;
 export const TIMEOUT = 10000;
-export const FILENAME = 'dog.jpg';
+export const FILENAME = "dog.jpg";
